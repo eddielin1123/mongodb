87f5bca0ab1b2d6612312e1429002876ede8a222
eddielin1123 <zdtb5678@gmail.com>
2020-07-12 22:57:09 +0800

    上傳mon的設定檔

---
+++ mongo-init.js
@@ -0,0 +1,77 @@
+db.createUser(
+    {
+        user: "teammate1",
+        pwd: "eb102",
+        roles: [
+            {
+                role: "readWrite",
+                db: "Whiskey"
+            }
+        ]
+    }
+);
+
+db.createUser(
+    {
+        user: "teammate2",
+        pwd: "eb102",
+        roles: [
+            {
+                role: "readWrite",
+                db: "Whiskey"
+            }
+        ]
+    }
+);
+
+db.createUser(
+    {
+        user: "teammate3",
+        pwd: "eb102",
+        roles: [
+            {
+                role: "readWrite",
+                db: "Whiskey"
+            }
+        ]
+    }
+);
+
+db.createUser(
+    {
+        user: "teammate4",
+        pwd: "eb102",
+        roles: [
+            {
+                role: "readWrite",
+                db: "Whiskey"
+            }
+        ]
+    }
+);
+
+db.createUser(
+    {
+        user: "teammate5",
+        pwd: "eb102",
+        roles: [
+            {
+                role: "readWrite",
+                db: "Whiskey"
+            }
+        ]
+    }
+);
+
+db.createUser(
+    {
+        user: "teammate6",
+        pwd: "eb102",
+        roles: [
+            {
+                role: "readWrite",
+                db: "Whiskey"
+            }
+        ]
+    }
+);
